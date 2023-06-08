e0e6bc8b2e962c44874e8a0bb192df36380319c5
Pratham Vaidya <iamprathamvaidya@gmail.com>
2023-06-08 14:50:57 +0530

    Added Responsiveness in Disc

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,45 @@ import Slider from "./components/Slider";
 
 function App() {
   return (
-    <div className="flex w-screen h-screen bg-background">
-      <div className="h-1/2 w-full grid grid-cols-5 items-center justify-center relative">
-        <div className="col-span-2">
-          <Disc className="absolute left-0 top-0" title="Kya Hua Tera Wada" />
+    <div className="w-screen h-screen bg-background overflow-hidden">
+      {/* Upper Panel with Disc and Sliders */}
+      <div className="h-[60vh] w-full grid grid-cols-3 items-center justify-center">
+        <div className="h-full">
+          <div className="h-full w-full relative overflow-hidden">
+            <Disc
+              className="absolute right-0 bottom-0"
+              title="Kya Hua Tera Wada"
+            />
+          </div>
         </div>
-        <div className="h-full flex flex-col gap-8">
+        <div className="h-full flex flex-col gap-4 p-4">
           <div className="h-full flex justify-around">
-            <div className="h-full w-20 rounded-lg bg-lightBlack m-4 flex justify-center p-4">
+            <div className="h-full w-20 rounded-lg bg-lightBlack flex justify-center p-4">
               <Slider />
             </div>
-            <div className="h-full w-20 rounded-lg bg-lightBlack m-4 flex justify-center p-4">
+            <div className="h-full w-20 rounded-lg bg-lightBlack flex justify-center p-4">
               <Slider />
             </div>
           </div>
-          <div>
-            <div className="h-full w-full rounded-lg bg-lightBlack m-4 flex items-center p-4">
-              <Slider orientation="horizontal" thumbType="square" />
-            </div>
+          <div className="h-f w-full rounded-lg bg-lightBlack flex items-center p-8">
+            <Slider orientation="horizontal" thumbType="square" />
           </div>
         </div>
-        <div className="col-span-2">
-          <Disc className="absolute right-0 top-0" />
+        <div className="h-full relative">
+          <div className="h-full w-full relative overflow-hidden">
+            <Disc
+              className="absolute left-0 bottom-0 "
+              title="Perfect - Ed Sheeren"
+            />
+          </div>
         </div>
+      </div>
+
+      {/* Lower Panel with Search , Song List and Round Controllers */}
+      <div className="h-[40vh] w-full grid grid-cols-3 items-center justify-center">
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
--- src/components/Disc.jsx
+++ src/components/Disc.jsx
@@ -1,16 +1,29 @@
 function Disc({ className, title, status = "playing" }) {
+  // Add more breakpoints for width , height and circle size to make it more responsive.
   return (
     <div
       className={
-        "w-[16rem] h-[16rem] rounded-full outline-[16rem] outline-primary outline m-[2rem] cursor-pointer  " +
+        "w-[25rem] h-[25rem] md:w-[45rem] md:h-[45rem] rounded-full bg-primary overflow-hidden cursor-grab " +
         (status === "playing" ? "disc-animation " : " ") +
         className
       }
     >
-      <div className="absolute text-white font-bold -bottom-[10rem] left-[5rem] opacity-50 -rotate-90">
+      <div className="absolute top-1/2 left-1/2 w-[15rem] h-[15rem] bg-background rounded-full -translate-x-1/2 -translate-y-1/2 cursor-default" />
+      <div className="absolute text-white font-bold top-1/2 left-1/2 p-1 opacity-50 translate-x-32 -translate-y-1/2 w-[14rem] text-center">
         {title}
       </div>
     </div>
+    // <div
+    //   className={
+    //     "w-[16rem] h-[16rem] rounded-full outline-[16rem] outline-primary outline m-[2rem] cursor-pointer  " +
+    //     (status === "playing" ? "disc-animation " : " ") +
+    //     className
+    //   }
+    // >
+    //   <div className="absolute text-white font-bold -bottom-[10rem] left-[5rem] opacity-50 -rotate-90">
+    //     {title}
+    //   </div>
+    // </div>
   );
 }
 
